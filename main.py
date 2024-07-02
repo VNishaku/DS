6b67d085f47edf42b54152016e9c5a6ce38e493a
VNishaku <nk5766656@gmail.com>
2024-07-02 22:45:32 +0530

    first commit

--- main.py
+++ main.py
@@ -8,13 +8,15 @@
 # pitchers = [('Nolan', 'Ryan'), ('Roger', 'Clemens'),('Schilling', 'Curt')]
 # first_names, last_names = zip(*pitchers)
 # print(first_names)
-states = [' Alabama ', 'Georgia!', 'Georgia', 'georgia', 'FlOrIda', 'south carolina##', 'West virginia?']
-import re
-def clean_strings(strings):
- result = []
- for value in strings:
- value = value.strip()
- value = re.sub('[!#?]', '', value)
- value = value.title()
- result.append(value)
- return result
+# states = [' Alabama ', 'Georgia!', 'Georgia', 'georgia', 'FlOrIda', 'south carolina##', 'West virginia?']
+# import re
+# def clean_strings(strings):
+#  result = []
+#  for value in strings:
+#  value = value.strip()
+#  value = re.sub('[!#?]', '', value)
+#  value = value.title()
+#  result.append(value)
+#  return result
+import tensorflow as tf
+print(tf.__version__)
