437c4d12068b8ccb2448764faa5314c708dff731
Nisha kumari <104026732+VNishaku@users.noreply.github.com>
2024-07-22 17:13:58 +0000

    change

--- oops.py
+++ oops.py
@@ -1,12 +1,17 @@
 class Employee:
+    N0_of_Employ=0
     def __init__(self,first,last,pay):
         self.first=first
         self.last=last
         self.pay=pay
         self.email=first+'.'+last+'@commpany.com'
+
+        Employee.N0_of_Employ+=1
     def fullname(self):
         return '{}{}'.format(self.first,self.last)
     
 emp1=Employee('spliz','man','1300')
+emp2=Employee('Dorota','wyon',1900)
+emp3=Employee('Cactus','tanwar',9999)
 print(emp1.email)
-print(emp1.fullname())
+print(Employee.N0_of_Employ)
